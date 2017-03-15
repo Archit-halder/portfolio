8025ccf43b0f92d090384602637f767d6f2a1313
ab-hi <bansal.abhishek101@gmail.com>
2017-03-15 18:39:30 +0530

    third draft

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,4 +4,16 @@
 		event.preventDefault();
 		$('html,body').animate({scrollTop:$(this.hash).offset().top}, 1000, 'easeOutBounce');
 	});
-  });
+
+  var animated_text = ['hello' , 'hi', 'how are you'];
+$('.test').text(animated_text[0]);
+var i=1;
+var timeInterval = 1000;
+setInterval(function() {
+   $('.test').text(animated_text[i]);
+	i = i+1;
+if(i>animated_text.length){i=0;}
+}, timeInterval);
+
+
+ });
